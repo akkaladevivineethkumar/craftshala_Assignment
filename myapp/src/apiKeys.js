03d6fbc1c2e295d988d4135d15654c53173e7783
Akkaladevi Vineeth <123940760+akkaladevivineethkumar@users.noreply.github.com>
2024-06-15 14:40:56 +0530

    Assignment

---
+++ myapp/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "557448d30dcc71eb3b591672b4626cba",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
